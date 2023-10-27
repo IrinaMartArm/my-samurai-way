50b491a32012fcf2667a12106b67f596ceb6d7aa
Ирина Мартоян <surenira@icloud.com>
2023-10-27 18:40:39 +0400

    stor

--- src/App.tsx
+++ src/App.tsx
@@ -6,86 +6,41 @@ import { StyledApp } from "./styles/StyledApp";
 import { ProfilePage } from "./layaut/main/profilePage/ProfilePage";
 import styled from "styled-components";
 import { Theme } from "./styles/Theme";
-import { BrowserRouter, Route, Router } from "react-router-dom";
-import { ProfileInfo } from "./layaut/main/profilePage/profileInfo/ProfileInfo";
+import { BrowserRouter, Route } from "react-router-dom";
 import { Dialogs } from "./layaut/main/dialogs/Dialogs";
 import { News } from "./layaut/main/news/News";
-import { v1 } from "uuid";
+import {state} from "./state";
 
-type MessagesType = {
-  id: string
-  text: string
-}
 
-export type ContactType = {
-  id: string
-  name: string
-  messages: MessagesType[]
-}
-type ContactsType = ContactType[]
-
-export type PostType = {
-  id: string
-  post: string
-  likes: number
-}
-
-export type PostsType = PostType[] 
 
 
 function App() {
 
-  const contacts:  ContactsType = [
-    {id: v1(), 
-    name: 'Ira',  
-    messages: [
-        {id: v1(), text: 'hi'},
-        {id: v1(), text: 'hi'},
-        {id: v1(), text: 'hi'},
-    ]},
-    {id: v1(), 
-      name: 'Suren', 
-      messages: [
-        {id: v1(), text: 'hello'}
-    ]},
-    {id: v1(), 
-      name: 'Arina', 
-      messages: [
-        {id: v1(), text: 'how are you'}
-    ]},
-    {id: v1(), 
-      name: 'Liana', 
-      messages: [
-        {id: v1(), text: 'ok'}
-    ]},
-  ]
-
-  const posts: PostsType = [
-    {id: v1(), post: 'hi', likes: 28},
-    {id: v1(), post: 'hello', likes: 28},
-    {id: v1(), post: 'yoyo', likes: 28},
-]
-
-
-  return (
-    <BrowserRouter>
-      <StyledApp>
-        <Header />
-        <Aside />
-        <Main>
-          <Route path={"/profile"} render={()=> <ProfilePage posts={posts}/>} />
-          {/* <Route path={"/dialogs"} component={ <Dialogs contacts={contacts}/>} /> */}
-          <Route path={"/dialogs"} render={() => <Dialogs contacts={contacts}/>} />
-          <Route path={"/news"} render={() => <News />} />
-        </Main>
-      </StyledApp>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+        <StyledApp>
+            <Header />
+            <Aside />
+            <Main>
+            <Route
+                path={"/profile"}
+                render={() => <ProfilePage posts={state.posts} />}
+            />
+            {/* <Route path={"/dialogs"} component={ <Dialogs contacts={contacts}/>} /> */}
+            <Route
+                path={"/dialogs"}
+                render={() => <Dialogs contacts={state.contacts} />}
+            />
+            <Route path={"/news"} render={() => <News />} />
+            </Main>
+        </StyledApp>
+        </BrowserRouter>
+    );
 }
 
 const Main = styled.main`
-  background-color: ${Theme.colors.primary};
-  grid-area: m;
+    background-color: ${Theme.colors.primary};
+    grid-area: m;
 `;
 
 export default App;
